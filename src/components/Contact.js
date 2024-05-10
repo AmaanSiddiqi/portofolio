02f4129e354174268963c12a29c100b676c99699
amaansiddiqi <amaansiddiqi2@gmail.com>
2024-05-09 21:32:23 -0700

    reorg

--- src/components/Contact.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Contact() {
-  return <div>Welcome to My Portfolio!</div>;
-}
-
-export default Contact;
--- src/components/Projects.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Projects() {
-  return <div>Welcome to My Portfolio!</div>;
-}
-
-export default Projects;
